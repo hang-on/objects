6a362afe49d472e710a28951bbc49486ffa1bd54
hang-on <hang-on@users.noreply.github.com>
2022-04-10 07:29:00 +0200

    u

--- tools/printMetaData.js
+++ tools/printMetaData.js
@@ -1,10 +1,11 @@
 // Print meta data of given image to the console.
 // In: Image filename as a command line argument.
+// Example syntax in cmd: node tools/printMetadata gfx/bird.png
 
 const sharp = require("sharp");
 const inFile = process.argv[2];
 
-async function getMetadata() {
+async function printMetadata() {
   try {
     const metadata = await sharp(inFile).metadata();
     console.log(metadata);
@@ -13,4 +14,4 @@ async function getMetadata() {
   }
 }
 
-getMetadata();
+printMetadata();
